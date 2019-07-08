02e16435f21a45b866ab12135942585c1e401fa5
Thumster <50561219+Thumster@users.noreply.github.com>
2019-07-08 15:59:29 +0800

    Facebook tryout
    simple facebook login. will need to add database and  logout

--- src/components/LoginPage/SignInBox/SignInBox.js
+++ src/components/LoginPage/SignInBox/SignInBox.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './SignInBox.css';
 import logo from './book_blue.png';
+import Facebook from './Facebook'
 
 class SignIn extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			logout:false
+		}
+	}
+
+	logout=()=>{
+		this.setState({logout:true});
+	}
+
 	render() {
 	return (
 		<div className="box">
 			<img className="logo" src={logo} />
 			<h1 className="logo-name">Tutoree</h1>
-			<input className="input username" type="text" placeholder="Username" />
+			{/* <input className="input username" type="text" placeholder="Username" />
 			<br/>
 			<input className="input password" type="password" placeholder="Password" />
 			<p className="forget">Forgot password?</p>
@@ -16,7 +28,11 @@ class SignIn extends React.Component {
 			<div className="or">
 			OR
 			</div>
-			<button className="button sign-up-button">Sign Up</button>
+			<button className="button sign-up-button">Sign Up</button> */}
+			<br/>
+			<Facebook logout={this.state.logout} />
+			<br/>
+			<button className="button sign-up-button" onClick={this.logout}>Log Out</button> 
 		</div>
 	);}
 }
