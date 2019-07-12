38120fb20f9ec9cf4287eb356e5d5356ebaf7bb7
sunaga97 <50560441+sunaga97@users.noreply.github.com>
2019-07-12 15:06:14 +0800

    added heart

--- src/components/Post/PostCard/PostCard.js
+++ src/components/Post/PostCard/PostCard.js
@@ -2,18 +2,36 @@ import React from 'react';
 import profilePhoto from './user_photo.png';
 import './PostCard.css';
 import mailIcon from './Mail-icon.png';
+import unfilledHeart from './heart_unfilled.png';
+import filledHeart from './heart_filled.png';
 
 const teacher = {
     photoUrl: { profilePhoto },
     title: "O level subjects by ex-RI boi",
     profile_name: "raffles-ichiban",
     price: "100/hr",
-    location: "Tiong Bharu"
+    location: "Tiong Bharu",
+    liked : false
 }
 
-const PostCard = ({post}) => {
-    return (
-        <div id="containerid" className="container">
+class PostCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.like = this.like.bind(this);
+        this.state = {
+            liked : false
+        }
+    }
+
+    like() {
+        this.setState({
+            liked: !this.state.liked
+        });
+    }
+
+    render() {
+        return (
+            <div id="containerid" className="container">
             <div className="row">
                 <div className="col-5">
                     <div className="row">
@@ -24,20 +42,21 @@ const PostCard = ({post}) => {
                     <div className="row">
                         <div className="flexbutton">
                             <button type="button" class="btn btn-warning"><img src={mailIcon} style={{height:"1.5rem"}}/></button>
-                            <button type="button" class="btn btn-secondary"><img src={mailIcon} style={{height:"1.5rem"}}/></button>
+                            <button type="button" class="btn btn-light"><img onClick={this.like} src={this.state.liked ? filledHeart : unfilledHeart} style={{height:"1.5rem"}}/></button>
                         </div>
                     </div>
                 </div>
                 <div className="col-7">
-                    <p className="title">Title {post.title}</p>
-                    <p className="name">Name: {post.profile_name}</p>
-                    <p className="price">Price: {post.price}</p>
-                    <p className="location">Location: {post.location}</p>
+                    <p className="title">Title {this.props.post.title}</p>
+                    <p className="name">Name: {this.props.post.profile_name}</p>
+                    <p className="price">Price: {this.props.post.price}</p>
+                    <p className="location">Location: {this.props.post.location}</p>
                 </div>
 
             </div>
         </div>
-    );
+        );
+    }
 }
 
 export default PostCard
