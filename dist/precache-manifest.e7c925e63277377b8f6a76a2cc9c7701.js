8c046bc8005f1671a29599a6b0ecfd8758996439
Thumster <50561219+Thumster@users.noreply.github.com>
2019-07-29 20:45:39 +0800

    deployed

---
+++ dist/precache-manifest.e7c925e63277377b8f6a76a2cc9c7701.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46474e7aeed2841ee59f33ba8e09fb42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd25395ead12a904a42",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ad0e89c1e7d174cc386f",
+    "url": "/static/css/main.ec847754.chunk.css"
+  },
+  {
+    "revision": "5bd25395ead12a904a42",
+    "url": "/static/js/2.3a505715.chunk.js"
+  },
+  {
+    "revision": "ad0e89c1e7d174cc386f",
+    "url": "/static/js/main.bd5e76d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79af93a7cc1c89ac3ade25e87ef789a7",
+    "url": "/static/media/Teacher-Classroom-Blog-Getty.79af93a7.jpg"
+  },
+  {
+    "revision": "1b7125895230e1bff35b469574f44a8f",
+    "url": "/static/media/book_blue.1b712589.png"
+  },
+  {
+    "revision": "536c225478873816e5a61095427c9aa9",
+    "url": "/static/media/book_white.536c2254.png"
+  }
+]);
